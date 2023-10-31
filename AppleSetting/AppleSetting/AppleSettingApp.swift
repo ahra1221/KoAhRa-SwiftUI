83e200682bcb5cdac0fbdb9c6ec1b6927209a317
ahra1221 <lizze011221@gmail.com>
2023-11-01 01:31:32 +0900

    [Setting] #1 - 프로젝트 세팅

---
+++ AppleSetting/AppleSetting/AppleSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSettingApp.swift
+//  AppleSetting
+//
+//  Created by 고아라 on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct AppleSettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
